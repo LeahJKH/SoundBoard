4a2b611a0fab01ee62c157d1a15966717d6cae72
LeahJKH <leahjb06.kodehode@gmail.com>
2025-01-04 11:00:29 +0100

    made stuff in js

--- app.js
+++ app.js
@@ -45,3 +45,23 @@ function AudioPlay(name) {
 bongo.addEventListener("click", () => {AudioPlay(soundInfo[0].audioFile)})
 drumstick.addEventListener("click", () => {AudioPlay(soundInfo[2].audioFile)})
 drumkit.addEventListener("click", () => {AudioPlay(soundInfo[1].audioFile)})
+
+function createTxtSound (name, key, cardName) {
+    if (cardName === soundCard) {
+        const p = document.createElement("p")
+        const text = document.createTextNode(`${name} = ${key}`)
+        p.append(text)
+        p.className = "key-txt"
+        cardName.appendChild(p)
+    } 
+    else {
+        const btn = document.createElement("button")
+        const text = document.createTextNode(`${name}`)
+        btn.append(text)
+        btn.className = "btn-sounds"
+        cardName.appendChild(btn)
+    }
+}
+
+soundInfo.forEach((item) => createTxtSound(item.nameOf, item.keyOf, soundCard))
+soundInfo.forEach((item) => createTxtSound(item.nameOf, item.keyOf, btns))
