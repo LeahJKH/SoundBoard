2800fcb21b0070a9dc569b58be85e70fa8af808d
LeahJKH <leahjb06.kodehode@gmail.com>
2025-01-04 11:20:57 +0100

    finished logic

--- app.js
+++ app.js
@@ -9,6 +9,8 @@ const soundCard = document.querySelector("#sound-card")
 const btns = document.querySelector("#btns")
 // Containers
 
+const randomBtn = document.querySelector("#random-btn")
+
 const soundInfo = [
     {
         nameOf: "bongo",
@@ -44,15 +46,27 @@ function AudioPlay(name) {
 
 bongo.addEventListener("click", () => {AudioPlay(soundInfo[0].audioFile)})
 drumstick.addEventListener("click", () => {AudioPlay(soundInfo[2].audioFile)})
-drumkit.addEventListener("click", () => {AudioPlay(soundInfo[1].audioFile)})
+drumkit.addEventListener("click", () => {
+    let random = Math.floor(Math.random() * soundInfo.length + 1)
+    if (random === 5) {
+        AudioPlay(soundInfo[4].audioFile)
+    } else {
+        AudioPlay(soundInfo[random].audioFile)
+    }
+})
 
-function createTxtSound (name, key, cardName) {
+function createTxtSound (name, key, cardName, audioFileSent) {
     if (cardName === soundCard) {
         const p = document.createElement("p")
         const text = document.createTextNode(`${name} = ${key}`)
         p.append(text)
         p.className = "key-txt"
         cardName.appendChild(p)
+        window.addEventListener("keydown", (e) => {
+            if (e.key.toLowerCase() === key.toLowerCase()) {
+                AudioPlay(audioFileSent)
+            }
+        })
     } 
     else {
         const btn = document.createElement("button")
@@ -60,8 +74,14 @@ function createTxtSound (name, key, cardName) {
         btn.append(text)
         btn.className = "btn-sounds"
         cardName.appendChild(btn)
+        
     }
 }
 
-soundInfo.forEach((item) => createTxtSound(item.nameOf, item.keyOf, soundCard))
-soundInfo.forEach((item) => createTxtSound(item.nameOf, item.keyOf, btns))
+randomBtn.addEventListener("click", () => {
+    let random = Math.floor(Math.random() * soundInfo.length)
+    AudioPlay(soundInfo[random].audioFile) 
+    })
+
+soundInfo.forEach((item) => createTxtSound(item.nameOf, item.keyOf, soundCard, item.audioFile))
+soundInfo.forEach((item) => createTxtSound(item.nameOf, item.keyOf, btns, item.audioFile))
